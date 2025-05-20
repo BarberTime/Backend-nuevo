f2fe9a12f67b74e2925a26879cb179e994ab02da
Juan <juanapazaquispe84@gmail.com>
2025-05-20 00:05:17 -0400

    ajustes3

--- config/settings.py
+++ config/settings.py
@@ -87,6 +87,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'apps.tenant.middleware.TenantDomainMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -177,6 +178,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
