2818a95843a1c56acffecd1db300a545e7e7f592
Juan <juanapazaquispe84@gmail.com>
2025-05-19 23:53:22 -0400

    ajustes2

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,8 @@ STATIC_ROOT = 'static/'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
 
 # CORS Configuration
 CORS_ALLOW_ALL_ORIGINS = True
@@ -129,16 +130,7 @@ DATABASES = {
     }
 }
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+
 
 
 # Password validation
